4a28d21790581b30ae65c485f3c8fa7249f0884d
Chad Altemose <caltemose@hugeinc.com>
2017-02-08 09:45:40 -0500

    removed all files, added walkthrough/tutorial html file for publishing to gh-pages

--- src/scripts/store/reducers/reducers.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-
-import items from './item-reducers'
-
-const reducers = combineReducers({ items })
-
-export default reducers
--- src/scripts/store/store-helpers.js
+++
@@ -1,3 +0,0 @@
-export const getErrorFromErrorResponse = (error, defaultError) => {
-    return error.response && error.response.data ? error.response.data.err : defaultError
-}
