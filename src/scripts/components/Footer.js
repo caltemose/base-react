966118a9cafdb4f83e8027cb179f83557e453681
Chad Altemose <caltemose@hugeinc.com>
2017-04-05 10:28:27 -0400

    added doc link and twitter link to footer

--- src/scripts/components/Footer.js
+++ src/scripts/components/Footer.js
@@ -7,6 +7,8 @@ const Footer = () => (
             <Link to="/">Home</Link>
             <Link to="/items">Items</Link>
             <a href="https://github.com/caltemose/base-react">Github repo for this tutorial</a>
+            <a href="https://github.com/caltemose/base-react">Documentation for this tutorial</a>
+            <a href="https://twitter.com/caltemose">Twitter Me</a>
         </nav>
     </footer>
 )
